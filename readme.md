f6d2f948fd943a7e92eca002b61249dc1d73ae99
virgilsisoe <sisoe24@gmail.com>
2021-10-18 11:55:00 -0400

    .

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # PROJECT README
+
+
+[![Coverage Status](https://coveralls.io/repos/github/sisoe24/GitActions/badge.svg?branch=main)](https://coveralls.io/github/sisoe24/GitActions?branch=main)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,16 @@
 from src.main import uppername
 
 
-def test_main():
-    name = uppername('virgil')
-    assert name.isupper()
+# def test_main():
+#     name = uppername('virgil')
+#     assert name.isupper()
 
 
-def test_main_upper():
-    name = uppername('virgil')
-    assert name == 'VIRGIL'
+# def test_main_upper():
+#     name = uppername('virgil')
+#     assert name == 'VIRGIL'
 
 
-def test_main_str():
-    name = uppername('virgil')
-    assert isinstance(name, str)
+# def test_main_str():
+#     name = uppername('virgil')
+#     assert isinstance(name, str)
