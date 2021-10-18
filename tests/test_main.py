8aaf9c6b9886ad121c79693e8d5bf860e973e46a
virgilsisoe <sisoe24@gmail.com>
2021-10-17 20:11:51 -0400

    .

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,12 @@ def test_main():
     name = uppername('virgil')
     assert name.isupper()
 
-def test_main_str():
+
+def test_main_upper():
     name = uppername('virgil')
     assert name == 'VIRGIL'
+
+
+def test_main_str():
+    name = uppername('virgil')
+    assert isinstance(name, str)
